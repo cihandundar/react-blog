601cc71a9ee7dcb1cc3d131e1c992f18f5fe24ff
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-21 19:16:15 +0300

    post page created

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchUser, handleDelete } from "../../features/user/userSlice";
 
 const Post = () => {
-  return <div>Post</div>;
+  const data = useSelector((state) => state?.users?.data);
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state?.users?.isLoading);
+
+  console.log(isLoading);
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  const handleDeleteFunction = (id) => {
+    dispatch(handleDelete(id));
+  };
+
+  return (
+    <main className="main">
+      <section className="section">
+        <div className="section__container">
+          <div className="section__container__title">
+            <h2>Post</h2>
+          </div>
+          <div className="container">
+            {isLoading ? (
+              <p className="loading">Loading...</p>
+            ) : (
+              data?.map((item) => (
+                <div className="cart" key={item?.id}>
+                  <Link to={`/post/${item?.id}`}>
+                    <div className="cart__title">
+                      <h2>{item?.name}</h2>
+                    </div>
+                    <div className="cart__job">
+                      <span>{item?.job}</span>
+                    </div>
+                    <div className="cart__description">
+                      <span>{item?.description}</span>
+                    </div>
+                    <div className="cart__category">
+                      <span>Category: {item?.category || "None"}</span>
+                    </div>
+                  </Link>
+                  <div className="cart__btn">
+                    <button
+                      className="delete"
+                      onClick={() => handleDeleteFunction(item?.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Post;
