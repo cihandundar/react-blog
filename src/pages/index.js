5d630da8fb17dcbfaf2c6e30c33a753e452b618d
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-21 19:15:32 +0300

    imported

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import AddPost from "./AddPost";
+import Home from "./Home";
 import Post from "./Post";
+import PostDetails from "./PostDetails";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
-export { AddPost, Post, SignIn, SignUp };
+export { AddPost, Post, SignIn, SignUp, PostDetails, Home };
