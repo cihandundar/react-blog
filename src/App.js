7ddb07c4999ccbf5af9d8e377f827005c8ca14b3
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-21 19:15:45 +0300

    imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Navbar } from "components";
-import { AddPost, Post, SignIn, SignUp } from "./pages";
+import { AddPost, Home, Post, PostDetails, SignIn, SignUp } from "./pages";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "components/PrivateRoute";
+import NotFoundError from "pages/Error";
 function App() {
   const route = [
     {
@@ -15,16 +16,24 @@ function App() {
       path: "/signup",
       element: <SignUp />,
     },
-    // {
-    //   path: "*",
-    //   element: <NotFoundError />,
-    // },
+    {
+      path: "*",
+      element: <NotFoundError />,
+    },
     {
       element: <PrivateRoute />,
       routes: [
         {
           path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/post",
           element: <Post />,
+        },
+        {
+          path: "/post/:id",
+          element: <PostDetails />,
         },
         {
           path: "/addpost",
